b48c0adebf76b5500473c9d7fdd2a2a3a0addfb3
Zi-Xuan Zhou <scizx@foxmail.com>
2026-06-03 22:14:14 -0400

    Expose neuronumba model classes in package init

--- nmp_modeling/adapters/neuronumba/models/__init__.py
+++ nmp_modeling/adapters/neuronumba/models/__init__.py
@@ -1,7 +1,9 @@
 from .generic_beidmf import GenericBEIDMF
 from .generic_mdmf import GenericMDMF
+from .generic_hopf import GenericHopf
 
 __all__ = [
     "GenericBEIDMF",
     "GenericMDMF",
+    "GenericHopf",
 ]
